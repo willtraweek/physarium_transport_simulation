adc6fa078515c6c64946bf7f4d8b0f74c6ac18b5
Will Traweek <will.traweek@gmail.com>
2021-03-31 12:43:49 -0500

    add basic main

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,47 @@
+extern crate glutin_window;
+extern crate graphics;
+extern crate opengl_graphics;
+extern crate piston;
+
+mod dish;
+mod cell;
+
+use glutin_window::GlutinWindow as Window;
+use opengl_graphics::{GlGraphics, OpenGL};
+use piston::event_loop::{EventSettings, Events};
+use piston::input::{RenderArgs, RenderEvent, UpdateArgs, UpdateEvent};
+use piston::window::WindowSettings;
+use piston::EventLoop;
+
+const MAX_FPS: u64 = 5;
+const WINDOW_WIDTH: u32 = 200;
+const WINDOW_HEIGHT: u32 = 400;
+const BOX_SIZE: f64 = WINDOW_HEIGHT as f64 / 2.0;
+
+
 fn main() {
-    println!("Hello, world!");
+    // Change this to OpenGL::V2_1 if not working.
+    let opengl = OpenGL::V3_2;
+
+    // Create an Glutin window.
+    let mut window: Window = WindowSettings::new("Physarium Transport Simulation", [WINDOW_WIDTH, WINDOW_HEIGHT])
+        .graphics_api(opengl)
+        .exit_on_esc(true)
+        .build()
+        .unwrap();
+
+    let mut dish = dish::Dish::new(WINDOW_WIDTH, WINDOW_HEIGHT, opengl);
+
+    let mut events = Events::new(EventSettings::new());
+    events.set_ups(MAX_FPS + 1);
+    events.set_max_fps(MAX_FPS + 1);
+    while let Some(e) = events.next(&mut window) {
+        if let Some(args) = e.render_args() {
+            dish.render(&args);
+        }
+
+        if let Some(args) = e.update_args() {
+            dish.update(&args);
+        }
+    }
 }
